d407305a7ded6864aa9b0337e33f59a8b6fb5748
HyperGamesDev <hypergamesdev@gmail.com>
2021-05-10 12:43:50 +0200

    Project 0.1
    - No steering / Android settings

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # unity-example-hyper
-An example HyperGamesDev project
+An example HyperGamesDev project setup
---
+++ unity-example-hyper/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour{
+    [Header("Variables")]
+    public float hp;
+    [Header("Other")]
+    public bool damaged;
+    public bool healed;
+    void Start(){
+        
+    }
+    void Update(){
+        
+    }
+}
